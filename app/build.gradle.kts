e88885127c340531bbc184c15508633ce30e73d4
Sean Larson <seanlarson@seans-mbp-2.lan>
2021-03-17 13:07:49 -0400

    Moved to KTS

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+android {
+    compileSdkVersion(30)
+    defaultConfig {
+        applicationId = "com.bluelampcreative.recipease"
+        minSdkVersion(21)
+        targetSdkVersion(30)
+        versionCode = 1 //once we get some automation in place we can replace this with env vars.
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("debug") {
+            isDebuggable = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "$project.rootDir/tools/proguard-rules.pro")
+        }
+
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        useIR = true
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.0.0-beta01"
+        kotlinCompilerVersion = "1.4.30"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("com.google.android.material:material:1.2.1")
+    implementation("androidx.compose.ui:ui:1.0.0-beta01")
+    implementation("androidx.compose.material:material:1.0.0-beta01")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0-beta01")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0-alpha06")
+    implementation("androidx.activity:activity-compose:1.3.0-alpha02")
+    testImplementation("junit:junit:4.+")
+    androidTestImplementation("androidx.test.ext:junit:1.1.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.0-beta01")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        jcenter()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.0-alpha10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        mavenCentral()
+    }
+}
